e4dc23ac8fa67f149147f81a01d4d122841621e4
Eugenio Barquín <eu.barquin@me.com>
2025-06-23 18:31:36 +0200

    Upodate main.py

--- src/api/main.py
+++ src/api/main.py
@@ -1,11 +1,5 @@
-# src/api/main.py
-
 from fastapi import FastAPI
-from src.api.routes import cli_routes
+from src.api.routes import cli_routes  # asegúrate del path correcto
 
 app = FastAPI()
 app.include_router(cli_routes.router)
-
-@app.get("/")
-def home():
-    return {"mensaje": "¡Tu API está funcionando correctamente en Render!"}
